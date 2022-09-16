ad0f06c46007cf052bbfae00f29f4e2e07137d72
Augusto <jesus_augga_99@icloud.com>
2022-09-16 17:22:50 -0500

    add routes to privacy and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/privacy_policy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#landing_page'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
